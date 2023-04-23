6813a24f7206af6fb3fd748c6ad2cc1260365357
Jeremias Storm <jerestorm@gmail.com>
2023-04-23 20:08:46 -0300

    se estilizan los estilos y se terminan las tarjetas 3d

--- js/ej11.js
+++ js/ej11.js
@@ -1,38 +1,52 @@
 "use strict";
 
-const poster = document.getElementById("poster");
+const cards = document.getElementsByClassName("card");
 
-const height = poster.clientHeight;
-const width = poster.clientWidth;
+const height = cards[0].clientHeight;
+const width = cards[0].clientWidth;
 
-poster.addEventListener('mouseover', () => {
 
-    poster.addEventListener('mousemove', (e) => cardEfect(e))
+function main() {
 
-});
+    for (const element of cards) {
+        element.addEventListener('mouseover', () => {
+            console.log('estoy arriba de la card');
+            element.addEventListener('mousemove', (e) => cardEfect(e, element));
+            element.addEventListener('mouseout', (e) => {
+                element.removeEventListener('mousemove', cardEfect);
+                cardEfectOut(e)
+            });
+        });
+    };
+}
+function cardEfectOut(e) {
+
+    let card = e.target;
+    const style_defect = `
+            perspective(300px)
+            scale(1)
+            rotateX(0)
+            rotateY(0)
+            `;
+    card.style.transition = 'box-shadow .2s,transform .5s';
+    card.style.transform = style_defect;
+}
 
 function cardEfect(e) {
+    let card = e.target;
     const { layerX, layerY } = e;
 
-    const yRotation = ((layerX - width / 2) / width) * 40;
-    const xRotation = ((layerY - height / 2) / height) * 40;
+    const yRotation = ((layerX - width / 2) / width) * 20;
+    const xRotation = ((layerY - height / 2) / height) * 20;
 
-    const string = `
-    perspective(500px)
-    scale(1.2)
+    const card_style = `
+    perspective(300px)
+    scale(1.1)
     rotateX(${xRotation}deg)
     rotateY(${yRotation}deg)
     `;
 
-    poster.style.transform = string;
+    card.style.transition = 'box-shadow .2s, transform';
+    card.style.transform = card_style;
 }
 
-poster.addEventListener('mouseout', () => {
-    poster.removeEventListener('mousemove', cardEfect)
-    poster.style.transform = `
-    perspective(500px)
-    scale(1)
-    rotateX(0)
-    rotateY(0)
-    `;
-})
